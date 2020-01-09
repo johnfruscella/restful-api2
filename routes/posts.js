c89fd1bff075805dbe4d18d4e0ba9af85586e4aa
johnfruscella <johnfruscella@gmail.com>
2020-01-09 16:43:24 -0500

    modified Posts.js file

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,15 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
     res.send('We are on posts now');
 });
 
-router.get('/specific', (req, res) => {
-    res.send('the specific post');
-});
 
+router.post('/', (req,res) => {
+    console.log(req.body);
+})
 
 
 module.exports = router;

5a8588539a7937c6478dec7d7b783950583bbdb7
johnfruscella <johnfruscella@gmail.com>
2020-01-09 20:46:40 -0500

    completed restful api project

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('We are on posts now');
+//Gets all the Posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
+//Sends a post
 router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
@@ -15,11 +22,40 @@ router.post('/', async (req, res) => {
         const savedPost = await post.save();
         res.json(savedPost);
     } catch (err) {
-            res.json({ message: err });
+        res.json({ message: err });
     }
 });
-    
 
+//Gets a SPECIFIC post
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
 
+//Delete a Post
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId });
+        res.json(removedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+//Update a post
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne({ _id: req.params.postId },
+            { $set: { title: req.body.title } }
+        );
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+})
 
 module.exports = router;
